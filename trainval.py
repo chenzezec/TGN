5f58136174400d9ef195c08455e9124c875a2ff1
chenzezec <814429359@qq.com>
2021-07-14 20:59:48 +0800

    update trainval.py

--- trainval.py
+++ trainval.py
@@ -3,11 +3,26 @@ import os
 import torch
 import yaml
 from torch.utils.data import DataLoader
+from utils import TrajectoryDataset
+from model import TrajectoryModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--test_set', default='eth')
 parser.add_argument('--obs_len', default=8)
 parser.add_argument('--seq_len', default=20)
+parser.add_argument('--num_epochs', default=300)
 
 args = parser.parse_args()
 
+train_set = TrajectoryDataset(args, iftrain=True)
+train_loader = DataLoader(train_set, batch_size=1, shuffle=True, num_workers=0)
+val_set = TrajectoryDataset(args, iftrain=False)
+val_loader = DataLoader(val_set, batch_size=1, shuffle=False, num_workers=0)
+
+model = TrajectoryModel(args, dropout=0.1)
+
+def train():
+    print('Training begin')
+    test_error, test_final_error = 0, 0
+    for epoch in range(args.num_epochs):
+        
